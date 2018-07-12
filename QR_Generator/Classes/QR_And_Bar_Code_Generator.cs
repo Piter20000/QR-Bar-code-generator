fd6f1ed5daf2880fc425c2025c86dc61bc9e4dbf
Piter20000 <41114664+Piter20000@users.noreply.github.com>
2018-07-12 08:57:56 +0200

    Initial commit

---
+++ QR_Generator/Classes/QR_And_Bar_Code_Generator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Generator.Classes
+{
+    class QR_And_Bar_Code_Generator
+    {
+        /// <summary>
+        /// Method create Barcode
+        /// </summary>
+        /// <param name="text"> Your text to code </param>
+        /// <returns> Return image </returns>
+        public System.Drawing.Image Generate_Bar_Code(string text)
+        {
+            Zen.Barcode.Code128BarcodeDraw Barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+
+            return Barcode.Draw(text, 50);
+        }
+
+        /// <summary>
+        /// Method create QRc ode
+        /// </summary>
+        /// <param name="text"> Your text to code </param>
+        /// <returns> Return image </returns>
+        public System.Drawing.Image Generate_QR_Code(string text)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw QRcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+
+            return QRcode.Draw(text, 50);
+        }
+    }
+}
---
+++ QR_Generator/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QR_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Button_Click(object sender, EventArgs e)
+        {
+            Classes.QR_And_Bar_Code_Generator Instance = new Classes.QR_And_Bar_Code_Generator();
+
+            pictureBox1.Image = Instance.Generate_Bar_Code("Piotr Murdzia, test Barcode generator");
+
+            pictureBox2.Image = Instance.Generate_QR_Code("Piotr Murdzia, test QR code generator");
+        }
+    }
+}
